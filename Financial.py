7d56f21d47cc814760d5fab33d7819d28f479a06
sjk5cu <120754075+sjk5cu@users.noreply.github.com>
2022-12-16 16:14:23 -0500

    Add files via upload

---
+++ Financial.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 12:20:30 2022
+
+@author: stephenkullman
+"""
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# Directory of this file
+this_dir = Path('/Users/stephenkullman/Desktop/All/Financials/CSV').resolve().parent
+
+# Read in all Excel files from folder
+parts = []
+
+for path in (this_dir).rglob("*.csv"):
+    part = pd.read_csv(path)
+    parts.append(part)
+    
+
+# Combine the DataFrames from each file into a single DataFrame # pandas takes care of properly aligning the columns
+df = pd.concat(parts)
+
+#Combining Chase and Apple csv value with different heads
+df["Amount"] = df["Amount"].fillna(df["Amount (USD)"] * -1)
+df.drop(['Memo','Amount (USD)','Purchased By','Clearing Date','Post Date'], axis = 1, inplace = True)
+
+
+
+# Reorganizing columns so amount is at the end
+cols = df.columns.tolist()
+cols = cols[:4] + cols[5:3:-1]
+df = df[cols]
+
+# Replacing some text
+df["Type"]= df["Type"].replace("Sale","Purchase")
+df = df.sort_values(by= "Transaction Date")
+
+df.to_excel("2022 Transactions.xlsx", sheet_name="Output", startrow=0, startcol=0, index=True, header=True, na_rep="<NA>", inf_rep="<INF>")
